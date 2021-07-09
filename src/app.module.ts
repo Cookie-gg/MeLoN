b6a7bc485d3841f20891b2185ab4eebc2c788378
Cookie <taruta1110@yahoo.co.jp>
2021-07-09 18:50:43 +0900

    add mongoDB and GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    MongooseModule.forRoot(
+      'mongodb+srv://cookie:v87j3ubQ8tJTFSzs@cluster0.rpjug.mongodb.net/test?retryWrites=true&w=majority',
+    ),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/cats/cats.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CatsResolver } from './cats.resolver';
+import { CatSchema } from './schemas/cats.schema';
+import { CatsService } from './cats.service';
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'Cat', schema: CatSchema }])],
+  providers: [CatsResolver, CatsService],
+})
+export class CatsModule {}
