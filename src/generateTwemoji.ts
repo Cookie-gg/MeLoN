4027ab1ab29e3384e8ea50085e19ca1af02482af
Cookie <taruta1110@yahoo.co.jp>
2021-12-30 17:55:44 +0900

    fixed path

--- src/generateTwemoji.ts
+++ src/generateTwemoji.ts
@@ -12,52 +12,52 @@ import * as dotenv from 'dotenv';
   });
   const readFile = util.promisify(fs.readFile);
   const writeFile = util.promisify(fs.writeFile);
-  // const twemojiSvg = await readFile(
-  //   path.join(
-  //     process.cwd(),
-  //     'node_modules/react-twemoji-picker/data/twemoji.svg',
-  //   ),
-  //   'utf-8',
-  // );
-  // const octokit = new Octokit({ auth: process.env.GITHUB_ACCESS_TOKEN });
-  // const owner = process.env.GITHUB_OWNER;
-  // const repo = 'LeMoN';
-  // const latestCommit = (
-  //   await octokit.rest.repos.getBranch({
-  //     owner,
-  //     repo,
-  //     branch: process.env.PORT ? 'master' : 'preview',
-  //   })
-  // ).data.commit;
+  const twemojiSvg = await readFile(
+    path.join(
+      process.cwd(),
+      'node_modules/react-twemoji-picker/data/twemoji.svg',
+    ),
+    'utf-8',
+  );
+  const octokit = new Octokit({ auth: process.env.GITHUB_ACCESS_TOKEN });
+  const owner = process.env.GITHUB_OWNER;
+  const repo = 'LeMoN';
+  const latestCommit = (
+    await octokit.rest.repos.getBranch({
+      owner,
+      repo,
+      branch: process.env.PORT ? 'master' : 'preview',
+    })
+  ).data.commit;
 
-  // const rootTree = (
-  //   await octokit.rest.git.getTree({ owner, repo, tree_sha: latestCommit.sha })
-  // ).data.tree;
+  const rootTree = (
+    await octokit.rest.git.getTree({ owner, repo, tree_sha: latestCommit.sha })
+  ).data.tree;
 
-  // const fileTree = (
-  //   await octokit.rest.git.getTree({
-  //     owner,
-  //     repo,
-  //     tree_sha: rootTree.find((file) => file.path === 'public')?.sha,
-  //   })
-  // ).data.tree;
+  const fileTree = (
+    await octokit.rest.git.getTree({
+      owner,
+      repo,
+      tree_sha: rootTree.find((file) => file.path === 'public')?.sha,
+    })
+  ).data.tree;
 
-  // const blob = (
-  //   await octokit.rest.git.getBlob({
-  //     owner,
-  //     repo,
-  //     file_sha: fileTree.find((file) => file.path === 'twiEmoji.svg')?.sha,
-  //   })
-  // ).data;
+  const blob = (
+    await octokit.rest.git.getBlob({
+      owner,
+      repo,
+      file_sha: fileTree.find((file) => file.path === 'twemoji.svg')?.sha,
+    })
+  ).data;
 
-  // await octokit.rest.repos.createOrUpdateFileContents({
-  //   owner,
-  //   repo,
-  //   path: 'public/twiEmoji.svg',
-  //   message: 'Update twiEmoji.svg',
-  //   content: Base64.encode(twemojiSvg),
-  //   sha: blob.sha,
-  // });
+  await octokit.rest.repos.createOrUpdateFileContents({
+    owner,
+    repo,
+    path: 'public/twemoji.svg',
+    message: 'Update twemoji.svg',
+    content: Base64.encode(twemojiSvg),
+    sha: blob.sha,
+  });
 
   const emojiSet: { [key: string]: string } = Object.assign(
     {},
