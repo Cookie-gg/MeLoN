69b86692e46737a9632f436767d565ed303bef4e
Cookie <taruta1110@yahoo.co.jp>
2022-01-28 21:09:19 +0900

    support preview front url

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: [`${process.env.FRONT_URL}`, /https:\/\/.*?cookie-gg\.[dev|vercel][?\.app]/],
+      origin: [`${process.env.FRONT_URL}`, /https:\/\/.*?cookie-gg\.(dev|vercel)(?(\.app))/],
       credentials: true,
     },
   });
