5712db4cc37554011efad2f98e9e814f294b14f7
Cookie <taruta1110@yahoo.co.jp>
2022-02-07 18:56:15 +0900

    added class-validate and updated modules

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({ logger: process.env.PORT !== undefined }),
-    {
-      cors: {
-        origin: [`${process.env.FRONT_URL}`, 'https://cookie-gg.dev', /https:\/\/.*?cookie-gg\.vercel\.app/],
-        credentials: true,
-      },
-    },
   );
+  app.enableCors({
+    origin: [`${process.env.FRONT_URL}`, 'https://cookie-gg.dev', /https:\/\/.*?cookie-gg\.vercel\.app/],
+    credentials: true,
+  });
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.PORT || 4000);
 }
