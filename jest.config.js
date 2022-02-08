706bdda3c9e1b770766d59feb0baa5e6934c0e9b
Cookie <taruta1110@yahoo.co.jp>
2022-02-08 17:21:05 +0900

    changed testRegex

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,15 @@
 module.exports = {
   moduleFileExtensions: ['js', 'json', 'ts'],
-  testRegex: '.*\\.spec\\.ts$',
+  testRegex: '.*\\.spec\\.ts',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
   testEnvironment: 'node',
   collectCoverageFrom: ['**/*.(t|j)s'],
   moduleDirectories: ['node_modules', '<rootDir>/src'],
+  coverageThreshold: {
+    global: {
+      lines: 80,
+    },
+  },
 };
